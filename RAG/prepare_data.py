d97a4e7e55e9706644ca614e6898935999a23c3b
Weiping Qiu <weiping@WeipingQiu.local>
2026-04-19 16:54:36 -0400

    embedding and vector.

--- RAG/prepare_data.py
+++ RAG/prepare_data.py
@@ -19,8 +19,56 @@ from ragas.metrics import (
     context_precision,
 )
 
+
+
+
 # 1. Load and Split file.
 loader = TextLoader("./documents/law.txt")
 # 
 documents = loader.load()
-print(documents)
+# print(documents)
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=30)
+splits = text_splitter.split_documents(documents)
+text_list = []
+for i in splits:
+    print(i)
+    text_list.append(i)
+
+
+
+prompt = PromptTemplate(
+    input_variables=["context", "query"],
+    template="""请根据检索的上下文回答问题。
+    
+    上下文：
+    {context}
+
+    问题：
+    {query}
+
+    答案："""
+)
+
+
+
+# embedding, and vector store
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="BAAI/bge-base-zh-v1.5",
+    model_kwargs={"device": "cpu"},
+    encode_kwargs={"normalize_embeddings": True}
+)
+
+
+vectorstore = Chroma.from_documents(
+                                    #文档切片
+                                    splits, 
+                                    #本地Embedding模型
+                                    embeddings,
+                                    #通过Chroma将向量存储到本地
+                                    persist_directory="./chrome_db"
+                                    )
+
+print(vectorstore)
